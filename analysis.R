9925664a62a1bc92aa82b3cc81cbdfab79b08bc0
njkpark <njkpark@uw.edu>
2022-02-22 20:37:09 -0800

    mean graphs

--- analysis.R
+++ analysis.R
@@ -16,6 +16,9 @@ deaths_covid <- read.csv("./data/deaths_covid.csv",
   select(Year, Month, All.Cause, Intentional.Self.Harm..Suicide.) %>%
   mutate(suicide_percent_total_deaths = (Intentional.Self.Harm..Suicide. / All.Cause) * 100)
 
+mental_care <- read.csv("./data/mental_care.csv",
+                        stringsAsFactors = FALSE) 
+
 ## Combines both Deaths dataframes into one.
 deaths_pre_post_covid <- rbind(deaths_pre_covid, deaths_covid)
 deaths_pre_post_covid$Date <- as.yearmon(paste(deaths_pre_post_covid$Year, deaths_pre_post_covid$Month), "%Y %m")
@@ -31,4 +34,38 @@ suicide_death_plot <- ggplot(deaths_pre_post_covid, aes(x = Date, y = Intentiona
 suicide_percent_plot <- ggplot(deaths_pre_post_covid, aes(x = Date, y = suicide_percent_total_deaths, group = 1)) +
   geom_line() +
   geom_point()
+
+## Calculating and graphing the mean suicide rates precovid and 2020
+pre_mean <- mean(deaths_pre_covid$Intentional.Self.Harm..Suicide.)
+covid_mean <- mean(deaths_covid$Intentional.Self.Harm..Suicide., na.rm = TRUE)
+
+pre_mean_percent <- mean(deaths_pre_covid$suicide_percent_total_deaths)
+covid_mean_percent <- mean(deaths_covid$suicide_percent_total_deaths, na.rm = TRUE)
+
+#Making dataframe from values to graph
+covid <- c("Pre-Covid", "Covid")
+mean_deaths <- c(pre_mean, covid_mean)
+
+mean_deaths_df <- data.frame(covid, mean_deaths)
+
+#percent dataframe
+perc_deaths <- c(pre_mean_percent, covid_mean_percent)
+perc_deaths_df <- data.frame(covid, perc_deaths)
+
+#Graphs
+mean_plot <- ggplot(mean_deaths_df, aes(x = covid, y = mean_deaths)) +
+  geom_bar(stat="identity",position="dodge") +
+  scale_fill_discrete(name="Year",
+                      breaks=c(1, 2),
+                      labels=c("Pre-Covid", "Covid"))+
+  xlab("Years")+ylab("Death Means")
+## the means do not differ, if anything, covid deaths were slightly more prevalent
+
+perc_plot <- ggplot(perc_deaths_df, aes(x = covid, y = perc_deaths)) +
+  geom_bar(stat="identity",position="dodge") +
+  scale_fill_discrete(name="Year",
+                      breaks=c(1, 2),
+                      labels=c("Pre-Covid", "Covid"))+
+  xlab("Years")+ylab("Percent Death Means")
+## the means of death percentages show a significant difference between pre-covid and covid years 
 
